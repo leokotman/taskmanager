b74f445252fb27514f95c29427f6abb01c14b69a
leokot <leoktmn@yandex.ru>
2022-11-09 18:55:01 +0100

    fix: edit rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
@@ -13,7 +15,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 end
