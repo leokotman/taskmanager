7294f4dd3a0dc28f3d1e53c77dac8c0390106541
leokot <leoktmn@yandex.ru>
2022-11-10 17:48:32 +0100

    feat: add state to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,37 @@
 class Task < ApplicationRecord
+  state_machine :state, initial: :new_task do
+    state :in_development
+    state :archived
+    state :in_qa
+    state :in_code_review
+    state :ready_for_release
+    state :released
+
+    event :start_develop do
+      transition [:new_task, :in_code_review, :in_qa] => :in_development
+    end
+
+    event :send_to_qa do
+      transition in_development: :in_qa
+    end
+
+    event :send_to_code_review do
+      transition in_qa: :in_code_review
+    end
+
+    event :prepare_to_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :send_to_release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+  end
+
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
