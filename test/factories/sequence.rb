e4890e256e5db9be6b10f88214d4254005fd3639
leokot <leoktmn@yandex.ru>
2022-11-10 13:35:27 +0100

    feat: create models&factories tests

---
+++ test/factories/sequence.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+    "string#{
+      n
+    }"
+  end
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+  sequence :avatar do |n|
+    "#{:first_name}#{n}.jpg"
+  end
+  sequence :type do |n|
+    "developer"
+  end
+end
