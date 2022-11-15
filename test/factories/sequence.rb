85c5ee595dafd8fb4e3efd2d234e575368de8de2
leokot <leoktmn@yandex.ru>
2022-11-15 16:25:47 +0100

    fix: edit sequence linebreak bug

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   sequence :string, aliases: [:first_name, :last_name, :password] do |n|
-    "string#{
-      n
-    }"
+    "string#{n}"
   end
   sequence :email do |n|
     "person#{n}@example.com"
