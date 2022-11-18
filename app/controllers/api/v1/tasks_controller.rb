8ca3215078811bf0564c9cce52ef79613f316a9b
leokot <leoktmn@yandex.ru>
2022-11-18 13:30:52 +0100

    feat: api get show task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,6 +3,13 @@ class Api::V1::TasksController < Api::V1::ApplicationController
   def index
     tasks = Task.all.ransack(ransack_params).result.page(page).per(per_page)
 
-    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+    respond_with(tasks, each_serializer: :TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+  # GET /api/v1/tasks/:id
+  def show
+    task = Task.find(params[:id])
+
+    respond_with(task, serializer: :TaskSerializer)
   end
 end
