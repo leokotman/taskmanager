1d7e85fb3c485997526d27a8ca31beed715d11c2
leokot <leoktmn@yandex.ru>
2022-11-18 12:36:51 +0100

    feat: api get index

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,8 @@
-class Api::V1::TasksController < ApplicationController
+class Api::V1::TasksController < Api::V1::ApplicationController
+  # GET /api/v1/tasks
+  def index
+    tasks = Task.all.ransack(ransack_params).result.page(page).per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
 end
