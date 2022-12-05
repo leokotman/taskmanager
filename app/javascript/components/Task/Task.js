78ea7fce414b14885b8ebd82acb096b5a98c654c
leokot <leoktmn@yandex.ru>
2022-12-05 19:20:21 +0100

    feat: render TaskBoard with tasks on board

---
+++ app/javascript/components/Task/Task.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Card, CardHeader, CardContent, Typography } from '@mui/material';
+
+import useStyles from './styles';
+
+const Task = (props) => {
+  const { task } = props;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title={task.name} />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {task.description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape().isRequired,
+};
+
+export default Task;
